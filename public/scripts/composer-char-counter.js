4eb5e9466d335f3939773c5e5538f88a5f66bd70
Charles P <charles.prystupa@gmail.com>
2020-11-17 22:58:19 +0000

    Implement Shadow over article, and username appears.  Correct char-counter to change CSS Class rather than direct style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,34 +1,21 @@
 $(document).ready(function () {
 
-
-  console.log("Doc is ready")
-
   $("#tweet-text").on('input', function() {
     //Logs The current Input (on input changes of anykind)
     const inputTextLength =$(this).val().length;
-    let remainingChars = 140-inputTextLength;
+    const remainingChars = 140-inputTextLength;
 
-    
+    //Add a class in HTML when triggered that will change the 
     if (remainingChars < 0){
-      $("output").css('color','red');
+      $("output").addClass('counterLimit')
+
     } else {
-      $("output").css('color','#545149');
+      $("output").removeClass('counterLimit')
     }
     //re-writes the char counter 
     $("output").val(remainingChars);
   })
 
  
-
-  // $('form button').on('keypress', () => {
-  //   console.log(this);
-  // })
-
-
-
-
-
-
-
 });
 
