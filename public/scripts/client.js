35df598bd0d789fb1d82d80c8e356c26c9edf852
Charles P <charles.prystupa@gmail.com>
2020-11-18 21:02:34 +0000

    Validate tweet before submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,75 +6,20 @@
 
 $(document).ready(function () {
 
+
   $('form button').on('click', event => {
     // Submit Post/Tweet
-    event.preventDefault()
-    $.ajax({
-      method: 'POST',
-      url: "/tweets/",
-      data: $('form').serialize()
-    })
-    //Clear Text Box after post and reset counter
-    $('#tweet-text').val('');
-    $('ouput.counter').val(140);
-  
+    event.preventDefault() 
+    validateAndSubmit()
+    
     //Add LatestPost to List
+
   });
-
-
-  const createTweetElement = function (post) {
-    // Got a good way of doing it, should break down into chunks
-    console.log("Creating Tweet", post)
-
-
-    const $tweet = $(`
-  
-  <article class="tweet">
-  <header>
-  <span><img src=${post.user.avatars}><p>${post.user.name}</p></span>
-  <span id="handle">${post.user.handle}</span>
-  </header>
-  <p class='tweet-contents'>${post.content.text}</p>
-  <footer>
-  <span>${post.created_at} days ago</span>
-  <span class="icons">
-    <i class="fas fa-flag" title="Flag Inappropriate"></i>
-    <i class="fas fa-retweet" title="Re-Twit"></i>
-    <i class="fas fa-heart" title="Heart it Up"></i>
-  </span>
-  </footer>
-  </article>
-  `);
-
-    // return $tweet;
-    $('#tweets-container').append($tweet);
-  };
-
-  const renderTweets = function (inputData, createMethod) {
-    console.log("Rendering (All data to indv)")
-    for (post of inputData) {
-      createTweetElement(post);
-    };
-  };
-
-
-
-
-
-
-  const loadTweets = function (action) {
-    console.log("LOADING TWEETS")
-    //GET the latest Tweet
-    $.ajax("/tweets/")
-      .then(res => {
-        console.log(res)
-        action(res)
-      });
-  };
 
   //Loads All Posts on REFRESH
   const fetchAndUpdateAll = () => loadTweets(renderTweets)
   fetchAndUpdateAll()
+
 
 
 });
---
+++ public/scripts/helper.js
@@ -0,0 +1,87 @@
+const loadTweets = function (action) {
+  console.log("LOADING ALL TWEETS")
+  //GET the latest Tweet
+  $.ajax("/tweets/")
+    .then(res => {
+      // console.log(res)
+      action(res)
+    });
+};
+
+const renderTweets = function (inputData) {
+  console.log("Rendering (All data to indv)")
+  for (post of inputData) {
+    createTweetElement(post);
+  };
+};
+
+
+const createTweetElement = function (post) {
+  // Got a good way of doing it, should break down into chunks
+  console.log("Creating Tweet", post)
+
+
+  const $tweet = $(`
+
+<article class="tweet">
+<header>
+<span><img src=${post.user.avatars}><p>${post.user.name}</p></span>
+<span id="handle">${post.user.handle}</span>
+</header>
+<p class='tweet-contents'>${post.content.text}</p>
+<footer>
+<span>${post.created_at} days ago</span>
+<span class="icons">
+  <i class="fas fa-flag" title="Flag Inappropriate"></i>
+  <i class="fas fa-retweet" title="Re-Twit"></i>
+  <i class="fas fa-heart" title="Heart it Up"></i>
+</span>
+</footer>
+</article>
+`);
+
+  // return $tweet;
+  $('#tweets-container').append($tweet);
+};
+
+
+const getLastTweet = (creationMethod) => {
+  const allData = loadTweets(creationMethod)
+  console.log("ALL>> ", allData)
+  const lastItem = Object.values(allData).pop()
+
+}
+
+const createLastTweet = (post) => {
+  createTweetElement(post);
+}
+
+
+
+const validateForm = function() {
+  input = $("#tweet-text").val()
+  if (input === "") {
+    // Must have something to input
+    alert("Tweets cannot be empty")
+
+  } else if (input.length > 140) {
+    console.log('Vinput', input)
+    //Max Character Exceeded
+    alert("Uh oh, your tweet is more than 140 characters")
+  } else {
+    submitTweet();
+  }
+   
+}
+
+const submitTweet = function(){
+  console.log('SUBMITTING')
+  $.ajax({
+    method: 'POST',
+    url: "/tweets/",
+    data: $('form').serialize()
+  })
+  //Clear Text Box after post and reset counter
+  $('#tweet-text').val('');
+  $('output.counter').val(140);
+}
