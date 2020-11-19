4b0b979c4bc6dd40b78cd002a7934b0be584d897
Charles P <charles.prystupa@gmail.com>
2020-11-19 22:34:36 +0000

    DeLinting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,20 +2,20 @@ $(document).ready(function () {
 
   $("#tweet-text").on('input', function() {
     //Logs The current Input (on input changes of anykind)
-    const inputTextLength =$(this).val().length;
-    const remainingChars = 140-inputTextLength;
+    const inputTextLength = $(this).val().length;
+    const remainingChars = 140 - inputTextLength;
 
-    //Add a class in HTML when triggered that will change the 
-    if (remainingChars < 0){
-      $("output").addClass('counterLimit')
+    //Add a class in HTML when triggered that will change the
+    if (remainingChars < 0) {
+      $("output").addClass('counterLimit');
 
     } else {
-      $("output").removeClass('counterLimit')
+      $("output").removeClass('counterLimit');
     }
-    //re-writes the char counter 
+    //re-writes the char counter
     $("output").val(remainingChars);
-  })
+  });
 
- 
+
 });
 
--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -1,33 +1,32 @@
-const loadTweets = function (action) {
+const loadTweets = function(action) {
   //GET the latest Tweet
   $.ajax("/tweets/")
     .then(res => {
-      action(res)
+      action(res);
     });
 };
 
-const renderTweets = function (inputData) {
+const renderTweets = function(inputData) {
   // empty then in sectoin
-  $('#tweets-container').empty()
+  $('#tweets-container').empty();
   // Reverses JSON to give newest first (should be switchable)
-  const revData = inputData //.reverse()
+  const revData = inputData; //.reverse()
   for (post of revData) {
     createTweetElement(post);
-  };
+  }
 };
 
 
-const createTweetElement = function (post) {
+const createTweetElement = function(post) {
   // Got a good way of doing it, should break down into chunks
 
   const $tweet = $(`
-
 <article class="tweet">
 <header>
 <span><img src=${post.user.avatars}><p>${post.user.name}</p></span>
 <span id="handle">${post.user.handle}</span>
 </header>
-<p class='tweet-contents'>${escape(post.content.text)}</p>  
+<p class='tweet-contents'>${escape(post.content.text)}</p>
 <footer>
 <span>${post.created_at} days ago</span>
 <span class="icons">
@@ -39,52 +38,48 @@ const createTweetElement = function (post) {
 </article>
 `);
 
-
-  // return $tweet;
   $('#tweets-container').prepend($tweet);
 };
 
-const escape = function (str) {
+const escape = function(str) {
   let p = document.createElement('p');
   p.appendChild(document.createTextNode(str));
   return p.innerHTML;
-}
+};
 
 
+const validateAndSubmit = function() {
 
+  $("#inputError").hide('slow');
+  const input = $("#tweet-text").val();
 
-const validateAndSubmit = function () {
- 
-  $("#inputError").hide('slow')
-  input = $("#tweet-text").val()
+  //Validate Before Submission
   if (input === "") {
     // Must have something to input
-        $('#inputError label').text("Womp womp, Tweets cannot be empty")
-        $('#inputError').show('slow')
+    $('#inputError label').text("Womp womp, Tweets cannot be empty");
+    $('#inputError').show('slow');
   } else if (input.length > 140) {
     //Max Character Exceeded
-    $('#inputError label').text("Whoa now! Your tweet is more than 140 characters")
-    $('#inputError').show('slow')
-  } else {
+    $('#inputError label').text("Whoa now! Your tweet is more than 140 characters");
+    $('#inputError').show('slow');
 
+  } else {
+    //POST and Tidy Up
     $.ajax({
       method: 'POST',
       url: "/tweets/",
       data: $('form').serialize()
     })
       .then(resetTextBox())
-      .then(loadTweets(renderTweets))
-    // .then(resetTextBox)
-    // .then($("#inputError").hide())
+      .then(loadTweets(renderTweets));
   }
-
-}
+};
 
 
 const resetTextBox = () => {
   //Clear Text Box after post and reset counter
   $('#tweet-text').val('');
   $('output.counter').val(140);
-}
+};
 
 
